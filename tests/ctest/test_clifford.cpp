ab06997128d55472ab8439eeab0c592569e36337
Joe Black <jrb00013wvu@gmail.com>
2026-07-06 17:56:01 -0400

    test: add clifford resonance unit test

---
+++ tests/ctest/test_clifford.cpp
@@ -0,0 +1,18 @@
+#include "crankle/crankle.h"
+
+#include <cmath>
+#include <cstdio>
+
+int main() {
+    crankle_multivector_t a{1.0, {1, 0, 0}, {0, 0, 0}, 0};
+    crankle_multivector_t b{1.0, {0, 1, 0}, {0, 0, 0}, 0};
+    uint64_t wa = crankle_crank_from_multivector(&a, 1);
+    uint64_t wb = crankle_crank_from_multivector(&b, 1);
+    double r = crankle_clifford_resonance(wa, wb);
+    if (std::fabs(r) > 10.0) {
+        std::printf("unexpected resonance %f\n", r);
+        return 1;
+    }
+    std::printf("test_clifford ok resonance=%f\n", r);
+    return 0;
+}
